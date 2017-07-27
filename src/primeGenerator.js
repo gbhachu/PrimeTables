87064cd5c82230b239984ee2b726ee5bb761fb91
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-07-27 08:55:25 +0100

    Refactored primeGenerator to return first 100 primes

--- src/primeGenerator.js
+++ src/primeGenerator.js
@@ -1,12 +1,10 @@
 // Generates an array with the first N prime numbers.
 
 function primeArrayGenerator(numberN){
-	var counter = 2;
 	var numberToTest = 3;
-	var primes = [];
-	primes[0] = 2;
-	while (counter<=numberN){
-		var isPrime=true;
+	var primes = [2];
+	while (primes.length < numberN){
+		var isPrime = true;
 		for ( var divisor=2; divisor<=Math.sqrt(numberToTest); divisor++){
 			if ((numberToTest % divisor) === 0) {
 				isPrime=false;
@@ -15,7 +13,6 @@ function primeArrayGenerator(numberN){
 		}
 		if (isPrime) {
 			primes.push(numberToTest);
-			counter=counter+1;
 		}
 		numberToTest=numberToTest+1;
 	}
